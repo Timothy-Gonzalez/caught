ab6b552077562ce7a15ea1f9c4925e831d2d6864
Timothy Gonzalez <105177619+Timothy-Gonzalez@users.noreply.github.com>
2024-02-04 04:29:44 -0600

    Add amalgamate tests target
    This should allow testing amalgamate locally

--- src/caught.h
+++ src/caught.h
@@ -1,6 +1,11 @@
+// The central entrypoint of caught
+// You should include this file from your tests
+// #include "caught.h"
+
 #ifndef CAUGHT_LIB
 #define CAUGHT_LIB
 
+#define _GNU_SOURCE // needed to make compiler happy, since this is the entrypoint
 #include "assertions.h"
 #include "tests.h"
 
