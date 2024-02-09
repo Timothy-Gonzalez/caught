86317b9c9190bf14a30288e2383a8df0cd33568c
Timothy Gonzalez <105177619+Timothy-Gonzalez@users.noreply.github.com>
2024-02-09 03:16:31 -0600

    Add array assertions

--- src/evaluators.c
+++ src/evaluators.c
@@ -17,6 +17,11 @@ bool caught_internal_evaluator_ptr_ptr(void **lhs, enum caught_operator operator
     CAUGHT_GENERATE_GENERIC_EVALUATOR_NULL_GUARD(lhs, operator, rhs);
     return caught_internal_evaluator_ptr(*lhs, operator, * rhs);
 }
+bool caught_internal_evaluator_ptr_array(void **lhs, enum caught_operator operator, void ** rhs, ssize_t length)
+{
+    CAUGHT_GENERATE_GENERIC_EVALUATOR_NULL_GUARD(lhs, operator, rhs);
+    CAUGHT_GENERATE_EVALUATOR_ARRAY(lhs, operator, rhs, length, caught_internal_evaluator_ptr);
+}
 
 bool caught_internal_evaluator_bool(bool lhs, enum caught_operator operator, bool rhs)
 {
@@ -26,6 +31,11 @@ bool caught_internal_evaluator_bool_ptr(bool *lhs, enum caught_operator operator
 {
     CAUGHT_GENERATE_GENERIC_EVALUATOR_NULL_GUARD(lhs, operator, rhs);
     return caught_internal_evaluator_bool(*lhs, operator, * rhs);
+}
+bool caught_internal_evaluator_bool_array(bool *lhs, enum caught_operator operator, bool * rhs, ssize_t length)
+{
+    CAUGHT_GENERATE_GENERIC_EVALUATOR_NULL_GUARD(lhs, operator, rhs);
+    CAUGHT_GENERATE_EVALUATOR_ARRAY(lhs, operator, rhs, length, caught_internal_evaluator_bool);
 }
 
 bool caught_internal_evaluator_int(int lhs, enum caught_operator operator, int rhs)
@@ -37,6 +47,11 @@ bool caught_internal_evaluator_int_ptr(int *lhs, enum caught_operator operator, 
     CAUGHT_GENERATE_GENERIC_EVALUATOR_NULL_GUARD(lhs, operator, rhs);
     return caught_internal_evaluator_int(*lhs, operator, * rhs);
 }
+bool caught_internal_evaluator_int_array(int *lhs, enum caught_operator operator, int * rhs, ssize_t length)
+{
+    CAUGHT_GENERATE_GENERIC_EVALUATOR_NULL_GUARD(lhs, operator, rhs);
+    CAUGHT_GENERATE_EVALUATOR_ARRAY(lhs, operator, rhs, length, caught_internal_evaluator_int);
+}
 
 bool caught_internal_evaluator_char(char lhs, enum caught_operator operator, char rhs)
 {
@@ -46,6 +61,11 @@ bool caught_internal_evaluator_char_ptr(char *lhs, enum caught_operator operator
 {
     CAUGHT_GENERATE_GENERIC_EVALUATOR_NULL_GUARD(lhs, operator, rhs);
     return caught_internal_evaluator_int(*lhs, operator, * rhs);
+}
+bool caught_internal_evaluator_char_array(char *lhs, enum caught_operator operator, char * rhs, ssize_t length)
+{
+    CAUGHT_GENERATE_GENERIC_EVALUATOR_NULL_GUARD(lhs, operator, rhs);
+    CAUGHT_GENERATE_EVALUATOR_ARRAY_ALLOW_NULL_TERMINATOR(lhs, operator, rhs, length, caught_internal_evaluator_char, '\0');
 }
 
 bool caught_internal_evaluator_str(char *lhs, enum caught_operator operator, char * rhs)
@@ -66,6 +86,11 @@ bool caught_internal_evaluator_str_ptr(char **lhs, enum caught_operator operator
 {
     CAUGHT_GENERATE_GENERIC_EVALUATOR_NULL_GUARD(lhs, operator, rhs);
     return caught_internal_evaluator_str(*lhs, operator, * rhs);
+}
+bool caught_internal_evaluator_str_array(char **lhs, enum caught_operator operator, char ** rhs, ssize_t length)
+{
+    CAUGHT_GENERATE_GENERIC_EVALUATOR_NULL_GUARD(lhs, operator, rhs);
+    CAUGHT_GENERATE_EVALUATOR_ARRAY_ALLOW_NULL_TERMINATOR(lhs, operator, rhs, length, caught_internal_evaluator_str, NULL);
 }
 
 bool caught_internal_evaluator_exit_status(caught_internal_process_status lhs, enum caught_operator operator, caught_internal_process_status rhs)
--- src/formatters.h
+++ src/formatters.h
@@ -2,27 +2,36 @@
 #include <string.h>
 #include <stdbool.h>
 #include "fork.h"
+#include "assertion-result.h"
 
 #ifndef CAUGHT_FORMATTERS
 #define CAUGHT_FORMATTERS
+
+#define CAUGHT_INTERNAL_FORMATTER_ARRAY_INITAL_SIZE 16
+#define CAUGHT_INTERNAL_FORMATTER_ARRAY_GROWTH_RATE 2
 
 // Formatters take a value of a certain type
 // they must return a dynamically allocated string that represents that value
 
 char *caught_internal_formatter_ptr(void *value);
 char *caught_internal_formatter_ptr_ptr(void **value);
+char *caught_internal_formatter_ptr_array(void **value, ssize_t length);
 
 char *caught_internal_formatter_bool(bool value);
 char *caught_internal_formatter_bool_ptr(bool *value);
+char *caught_internal_formatter_bool_array(bool *value, ssize_t length);
 
 char *caught_internal_formatter_int(int value);
 char *caught_internal_formatter_int_ptr(int *value);
+char *caught_internal_formatter_int_array(int *value, ssize_t length);
 
 char *caught_internal_formatter_char(char value);
 char *caught_internal_formatter_char_ptr(char *value);
+char *caught_internal_formatter_char_array(char *value, ssize_t length);
 
 char *caught_internal_formatter_str(char *value);
 char *caught_internal_formatter_str_ptr(char **value);
+char *caught_internal_formatter_str_array(char **value, ssize_t length);
 
 char *caught_internal_formatter_exit_status(caught_internal_process_status value);
 
@@ -35,5 +44,52 @@ char *caught_internal_formatter_exit_status(caught_internal_process_status value
     {                                               \
         return strdup("NULL");                      \
     }
+#define CAUGHT_INTERNAL_FORMATTER_ARRAY(array_exp, array_length, formatter) \
+    if (array_length <= 0)                                                  \
+    {                                                                       \
+        caught_output_errorf("Invalid length of array: %lu", array_length); \
+    }                                                                       \
+    char *result = malloc(CAUGHT_INTERNAL_FORMATTER_ARRAY_INITAL_SIZE);     \
+    size_t result_size = 0;                                                 \
+    size_t result_capacity = CAUGHT_INTERNAL_FORMATTER_ARRAY_INITAL_SIZE;   \
+    result[0] = '{';                                                        \
+    result[1] = ' ';                                                        \
+    result[2] = '\0';                                                       \
+    result_size += 2;                                                       \
+    int i;                                                                  \
+    for (i = 0; i < array_length; ++i)                                      \
+    {                                                                       \
+        char *formatted = formatter(array_exp[i]);                          \
+        if (i != 0)                                                         \
+        {                                                                   \
+            char *old_formatted = formatted;                                \
+            asprintf(&formatted, ", %s", old_formatted);                    \
+            free(old_formatted);                                            \
+        }                                                                   \
+        size_t size = strlen(formatted);                                    \
+        while (result_size + size + 3 > result_capacity)                    \
+        {                                                                   \
+            result_capacity *= CAUGHT_INTERNAL_FORMATTER_ARRAY_GROWTH_RATE; \
+            result = realloc(result, result_capacity);                      \
+        }                                                                   \
+        memcpy(result + result_size, formatted, size);                      \
+        result_size += size;                                                \
+        free(formatted);                                                    \
+    }                                                                       \
+    result[result_size] = ' ';                                              \
+    result[result_size + 1] = '}';                                          \
+    result[result_size + 2] = '\0';                                         \
+    return result;
+
+#define CAUGHT_INTERNAL_FORMATTER_ARRAY_ALLOW_NULL_TERMINATOR(array_exp, array_length, formatter, terminator) \
+    if (array_length < 0)                                                                                     \
+    {                                                                                                         \
+        array_length = -1;                                                                                    \
+        do                                                                                                    \
+        {                                                                                                     \
+            array_length += 1;                                                                                \
+        } while (array_exp[array_length] != terminator);                                                      \
+    }                                                                                                         \
+    CAUGHT_INTERNAL_FORMATTER_ARRAY(array_exp, array_length, formatter)
 
 #endif
